f803b2f0930c3f873cd9052915b606063dea8b3c
Aswun <aswinlutfian32@gmail.com>
2025-11-27 11:56:18 +0700

    membuat fun FormInputSiswa pada file HalamanEntry.kt

--- app/src/main/java/com/example/prak9/view/HalamanEntry.kt
+++ app/src/main/java/com/example/prak9/view/HalamanEntry.kt
@@ -1,12 +1,19 @@
 package com.example.prak9.view
 
+import androidx.compose.foundation.layout.*
 import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.input.nestedscroll.nestedScroll
+import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
+import com.example.prak9.R
+
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -68,5 +75,58 @@ fun EntrySiswaBody(
         ) {
             Text(text = stringResource(id = R.string.submit)) // Asumsi stringResource(id = "Submit") adalah R.string.submit
         }
+    }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun FormInputSiswa(
+    detailSiswa: DetailSiswa,
+    modifier: Modifier = Modifier,
+    onValueChange: (DetailSiswa) -> Unit = {},
+    enabled: Boolean = true
+) {
+    Column(
+        modifier = modifier,
+        verticalArrangement = Arrangement.spacedBy(space = dimensionResource(id = 16.dp))
+    ) {
+        OutlinedTextField(
+            value = detailSiswa.nama,
+            onValueChange = { onValueChange(detailSiswa.copy(nama = it)) },
+            label = { Text(text = stringResource(id = R.string.nama_siswa)) }, // Asumsi R.string.nama_siswa
+            modifier = Modifier.fillMaxWidth(),
+            enabled = enabled,
+            singleLine = true
+        )
+        OutlinedTextField(
+            value = detailSiswa.alamat,
+            onValueChange = { onValueChange(detailSiswa.copy(alamat = it)) },
+            label = { Text(text = stringResource(id = R.string.alamat_siswa)) }, // Asumsi R.string.alamat_siswa
+            modifier = Modifier.fillMaxWidth(),
+            enabled = enabled,
+            singleLine = true
+        )
+        OutlinedTextField(
+            value = detailSiswa.telpon,
+            onValueChange = { onValueChange(detailSiswa.copy(telpon = it)) },
+            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number),
+            label = { Text(text = stringResource(id = R.string.telpon_siswa)) }, // Asumsi R.string.telpon_siswa
+            modifier = Modifier.fillMaxWidth(),
+            enabled = enabled,
+            singleLine = true
+        )
+
+
+        if (enabled) {
+            Text(
+                text = stringResource(id = R.string.required_fields), // Asumsi R.string.required_fields
+                modifier = Modifier.padding(start = dimensionResource(id = 16.dp))
+            )
+        }
+        HorizontalDivider(
+            modifier = Modifier.padding(bottom = dimensionResource(id = 16.dp)),
+            thickness = dimensionResource(id = 8.dp),
+            color = Color.Blue
+        )
     }
 }
